64be1fd5c2766c83f0b82d8aac08d808edd890fd
Jake Lees <brocococonut@gmail.com>
2022-12-30 21:29:34 +1100

    Move declaration from main.ts to mod.ts

--- mod.ts
+++ mod.ts
@@ -1,8 +1,21 @@
+import type { JSX, VNode } from "preact";
 import { Plugin } from "$fresh/server.ts";
 
 import { Options, setup } from "./freshlate/shared.ts";
 
 export type { Options };
+
+declare module "preact" {
+  namespace JSX {
+    interface DOMAttributes<Target extends EventTarget> {
+      /**
+       * The translation key to use for the element's text content.
+       */
+      "data-t-key"?: string;
+      "data-t-key-params"?: Record<string, unknown>;
+    }
+  }
+}
 
 export default function translate(options: Options): Plugin {
   setup(options, undefined);
